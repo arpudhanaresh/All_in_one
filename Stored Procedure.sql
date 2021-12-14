5645d6ce31bfa598144575da583295ee94d3176a
Arpudha Naresh <59797552+arpudhanaresh@users.noreply.github.com>
2021-12-14 10:05:28 +0530

    Update Stored Procedure.sql

--- Stored Procedure.sql
+++ Stored Procedure.sql
@@ -4,3 +4,9 @@ Select * from STUDENT
 Select * from Student_log
 
 Exec sp_view_all
+
+**********************************************************************************************************
+
+Create procedure sp_view_parameter @stu_roll INT AS Select * from Student where stu_roll_no = @stu_roll;
+
+EXEC sp_view_parameter @stu_roll = 102
